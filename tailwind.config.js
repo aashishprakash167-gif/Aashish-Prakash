e27f6150b0931fb98e037b1e299a0ebdd9af9f24
aashishprakash167-gif <aashishprakash167@gmail.com>
2025-12-01 09:59:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
